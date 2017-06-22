45527607e1cb46a75b873717f6a44b1c3ecb977c
Jaakko Karhu <jaakkokarhu@Bomlers-MBP.lan>
2017-06-22 02:11:48 +0200

    add tests for nested, complex, filtered, add filter feature

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,120 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import PropPipe from '../src/index';
 
-import Starter from '../src/index';
+class Comp extends React.Component {
+    render() {
+        return <div>{this.props.children}</div>
+    }
+}
 
-test('Starter has text', () => {
+class NestedComp extends React.Component {
+    render() {
+        return (
+            <Comp>
+                <div className='nested' />
+            </Comp>
+        )
+    }
+}
 
-  const starter = shallow(
-    <Starter />
-  );
+const StatelessComp = (props) => {
+    return(
+        <div>{props.children}</div>
+    )
+}
 
-  expect(starter.text()).toEqual('React Component Boilerplate');
-  
+const NestedStatelessComp = (props) => {
+    return(
+        <StatelessComp>
+            <div />
+        </StatelessComp>
+    )
+}
+
+const Complex = () => {
+    return(
+        <span>
+            <Comp>
+                <div />
+            </Comp>
+            <Comp />
+            <div />
+            Just text.
+        </span>
+    )
+}
+
+const FilterComp = () => {
+    return(
+        <div className='data-needs-foo'>
+            <Comp>
+                <div className='data-needs-foo' />
+            </Comp>
+            <div />
+        </div>
+    )
+}
+
+test('Pipe stateful', () => {
+    const Piped = PropPipe(Comp, { 'data-foo': 'bar' })
+    const wrapper = shallow(
+        <Piped />
+    );
+    console.log(wrapper.html())
+    expect(wrapper.prop('data-foo')).toBe('bar')
 });
+
+test('Pipe nested stateful', () => {
+    const Piped = PropPipe(NestedComp, {'data-foo': 'bar'})
+    const wrapper = mount(
+        <Piped />
+    );
+    expect(wrapper.find('div').length).toBe(2)
+    expect(wrapper.find('[data-foo="bar"]').length).toBe(2)
+})
+
+test('Pipe stateless', () => {
+    const Piped = PropPipe(StatelessComp, {'data-foo': 'bar'})
+    const wrapper = mount(
+        <Piped />
+    )
+    expect(wrapper.find('div').length).toBe(1)
+    expect(wrapper.find('[data-foo="bar"]').length).toBe(1)
+})
+
+test('Pipe nested stateless', () => {
+    const Piped = PropPipe(NestedStatelessComp, {'data-foo': 'bar'})
+    const wrapper = mount(
+        <Piped />
+    )
+    expect(wrapper.find('div').length).toBe(2)
+    expect(wrapper.find('[data-foo="bar"]').length).toBe(2)
+})
+
+test('Pipe complex component (stateless)', () => {
+    const Piped = PropPipe(Complex, { 'data-foo': 'bar'})
+    const wrapper = mount(
+        <Piped />
+    )
+    expect(wrapper.find('div').length).toBe(4)
+    expect(wrapper.find('[data-foo="bar"]').length).toBe(5)
+})
+
+test('Pipe to filtered components (stateless)', () => {
+    const filter = (type, props={}) => {
+        if (props.className=='data-needs-foo') {
+            return true
+        } else {
+            return false
+        }
+    }
+    const Piped = PropPipe(FilterComp, {'data-foo': 'bar'}, filter)
+    const wrapper = mount(
+        <Piped />
+    )
+
+    expect(wrapper.find('div').length).toBe(4)
+    expect(wrapper.find('[data-foo="bar"]').length).toBe(2)
+
+})
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const getPipedTarget = (type, props) => {
-  console.log('PRÅPS', props)
+const getPipedTarget = (type, props={}, pipedProps, filter) => {
   let children
-  props = props
-  ? { ...props, className: props.className + ' red' }
-  : null
+  // Error handling here to chedk if pipedProps exist
+  props = filter(type, props)
+  ? { ...props, ...pipedProps }
+  : { ...props }
 
   if (Array.isArray(props.children))  { // multiple
     children = [ ...props.children ]
     for (let i in children) {
       const child = children[i]
-      children[i] = handleChild(child)
+      children[i] = handleChild(child, pipedProps, filter)
     }
   } else if (props.children) { // single
-    children = handleChild(props.children)
+    children = handleChild(props.children, pipedProps, filter)
   }
   if(typeof type=='function') {
-    const Piped = PropPipe(type)
+    const Piped = PropPipe(type, pipedProps, filter)
     return <Piped { ...props } />
   } else {
     const Elem = type
@@ -25,17 +25,17 @@ const getPipedTarget = (type, props) => {
   }
 }
 
-const handleChild = (child) => {
+const handleChild = (child, pipedProps, filter) => {
   if (!child) return
   if (isStateless(child.type)) { // Stateless component
-    const Piped = PropPipe(child.type)
+    const Piped = PropPipe(child.type, pipedProps, filter)
     return <Piped { ...child.props }/>
   } else if (typeof child.type == 'function') { // Stateful component
     const Child = MakeComp(child.type, child.props),
-          PipedChild = PropPipe(Child)
+          PipedChild = PropPipe(Child, pipedProps, filter)
     return <PipedChild />
   } else if (typeof child.type == 'string') { // Element
-    return getPipedTarget(child.type, child.props)
+    return getPipedTarget(child.type, child.props, pipedProps, filter)
   } else {
     return child
   }
@@ -47,15 +47,15 @@ const MakeComp = (Target, props) => class extends Target {
   render = () => <Target { ...props } />
 }
 
-const PropPipe = (Target, origin) => {
+const PropPipe = (Target, pipedProps={}, filter=() => true) => {
   if (!!Target.prototype.render) {
     return class PropPipe extends Target {
       constructor(props) {
         super(props)
         const rComp = this.render(),
-          { type } = rComp, // Prefix: r = rendered
-          rProps = rComp.props,
-          pipedTarget = getPipedTarget(type, rProps)
+              { type } = rComp, // Prefix: r = rendered
+              rProps = rComp.props,
+              pipedTarget = getPipedTarget(type, rProps, pipedProps, filter)
         this.render = () => {
           return(pipedTarget)
         }
@@ -66,7 +66,7 @@ const PropPipe = (Target, origin) => {
       constructor(props) {
         super(props)
         const target = Target(props || {}),
-              piped = getPipedTarget(target.type, target.props)
+              piped = getPipedTarget(target.type, target.props, pipedProps, filter)
         this.render = () => {
           return(piped)
         }

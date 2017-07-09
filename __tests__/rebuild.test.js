be40d96d17b9a6fa3dbb4330bd202ca0b0e54b3d
Jaakko Karhu <jaakkokarhu@Bomlers-MacBook-Pro.local>
2017-07-09 14:57:56 +0200

    test complex scenarios remove add key error

--- __tests__/rebuild.test.js
+++ __tests__/rebuild.test.js
@@ -32,10 +32,37 @@ class CompInsideComp extends React.Component {
     }
 }
 
-const Stateless = () => {
+const CompStl = (props) => {
     return (
         <div>
-            <span ref='reffedy_ref' />
+            { props.children }
+        </div>
+    )
+}
+
+const CompInsideElemStl = (props) => {
+    return (
+        <div>
+            <CompStl />
+        </div>
+    )
+}
+
+const CompInsideCompStl = (props) => {
+    return (
+        <CompStl>
+            <CompStl />
+        </CompStl>
+    )
+}
+
+const ArrInsideElemStl = (props) => {
+    return (
+        <div>
+            <p>Test paragraph</p>
+            <Comp />
+            <CompInsideElemStl />
+            Plain text
         </div>
     )
 }
@@ -52,10 +79,37 @@ test('Component inside of element (stateful)', () => {
     const wrapper = mount ( <Piped />)
     console.log('Component inside of element (stateful)', '\n\n', wrapper.html())
 })
+
 test('Component inside of component (stateful)', () => {
     const Piped = PropPipe(CompInsideComp)
     const wrapper = mount ( <Piped />)
     console.log('Component inside of component (stateful)', '\n\n', wrapper.html())
 })
+
+test('Basic component (stateless)', () => {
+    const Piped = PropPipe(CompStl)
+    const wrapper = mount( <Piped /> )
+    console.log('Basic component (stateless)', '\n\n', wrapper.html())
+})
+
+test('Component inside of element (stateless)', () => {
+    const Piped = PropPipe(CompInsideElemStl)
+    const wrapper = mount ( <Piped />)
+    console.log('Component inside of element (stateless)', '\n\n', wrapper.html())
+})
+
+test('Component inside of component (stateless)', () => {
+    const Piped = PropPipe(CompInsideCompStl)
+    const wrapper = mount ( <Piped />)
+    console.log('Component inside of component (stateless)', '\n\n', wrapper.html())
+})
+
+test('Array of children inside of element (stateless)', () => {
+    const Piped = PropPipe(ArrInsideElemStl)
+    const wrapper = mount( <Piped /> )
+    console.log('Array of children inside of element (stateless)', '\n\n', wrapper.html())
+})
+
 /*
+
 */
--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ const handleCloning = (target) => {
 	// console.log('TARGET', target)
 	const children = handleChildren(target.props.children)
 	const addedProps = { 'data-test-foo': 'bar' }
-	console.log('HANDLE CLONING')
 	if (typeof target.type=='string') {
 		return React.cloneElement(
 			target,
-			{ ...target.props, ...addedProps },
-			children
+			{ ...target.props, ...addedProps, children: children }
 		)
 	} else if (typeof target.type == 'function') {
 		const Target = React.createElement(PropPipe(target.type), { ...target.props, ...addedProps })
@@ -28,10 +26,8 @@ const handleChildren = (children) => { // Do I even need those props here
 
 const handleChild = (child) => {
  	if (typeof child == 'string') {
-  		console.log('STRING CHILD')
   		return child
   	} else {
-  		console.log('CHILD UNHANDLED')
   		return handleCloning(child)
   	}
 }
